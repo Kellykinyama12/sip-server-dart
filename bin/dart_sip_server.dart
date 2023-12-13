8710c274720d59262f488766e27632059f96344b
unknown <Administrator@IGLINTERNS5.zescocorp.zescoad.zesco.co.zm>
2023-12-13 15:13:15 +0200

    Files spelling corrected

--- bin/dart_sip_server.dart
+++ bin/dart_sip_server.dart
@@ -4,7 +4,7 @@ import 'wsSipServer.dart';
 //Function(dynamic resp)
 void main() {
   // ignore: unused_local_variable
-  SipServer sipServer = SipServer("0.0.0.0", 5081);
+  SipServer sipServer = SipServer("0.0.0.0", 5080);
   // wsSipServer wsServer =
   //     wsSipServer("192.168.0.90", 8088, "192.168.0.90", 5080);
 }
